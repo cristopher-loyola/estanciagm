d2755ef1bebff492d323955a7d7782f48f7ebfd0
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2025-03-29 14:08:07 -0600

    hasta aqui todo perfecto
    solo falta agregae la lectura de nitificaciones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,10 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __(' Inicio') }}
                     </x-nav-link>
                     <x-nav-link :href="route('calendar')" :active="request()->routeIs('calendar')">
-                        {{ __('Calendario') }}
+                        {{ __('Vacaciones') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -119,10 +119,10 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Inicio') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('calendar')" :active="request()->routeIs('calendar')">
-                {{ __('Calendario') }}
+                {{ __('Vacaciones') }}
             </x-responsive-nav-link>
         </div>
 
